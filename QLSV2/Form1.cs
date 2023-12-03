024443feaf2285560655b3443ef4d69d3a665a6f
VMNhut23 <vominhnhut151103@gmail.com>
2023-12-04 01:20:12 +0700

    first commit

---
+++ QLSV2/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace QLSV2
+{
+	public partial class Form1 : Form
+	{
+		Student st = new Student();
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			DataStudentDataContext db = new DataStudentDataContext();
+			var lst = (from st in db.Students select st).ToList();
+			dataGridView1.DataSource = lst;
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			DataStudentDataContext db = new DataStudentDataContext();
+			st.id = textBox1.Text;
+			st.fullname = textBox2.Text;
+			st.dob = textBox3.Text;
+			db.Students.InsertOnSubmit(st);
+			db.SubmitChanges();
+			button1_Click(sender, e);
+		}
+	}
+}
